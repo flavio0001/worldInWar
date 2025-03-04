22575ea873b40397ba2e2d63b9f6d19bb1c81d8f
Luan Alves <luanlisboa220@gmail.com>
2025-03-04 20:10:16 -0300

    adicionando a janela do jogo, Game.py

---
+++ WW/src/cliente/game.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.width, self.height = 800, 600
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("WorldInWar")
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
